6f1cd34f8a1b10840d3e44d772e4be7b1a9bd274
Mustosoft <mustosoftware@gmail.com>
2024-03-16 18:58:46 +0000

    feat: add popup for no edit

--- bot/controller/incomingCallbacks.js
+++ bot/controller/incomingCallbacks.js
@@ -5,6 +5,9 @@
 
 const { C, U } = require('../consts/char');
 
+const rU = RegExp(`^${U}`);
+const rC = RegExp(`^${C}`);
+
 // List of pair of regex and function to handle incoming messages
 /**
  * @type {PatternHandlers}
@@ -16,6 +19,8 @@ module.exports = [
             const msg = await event.getMessage();
             const checkAll = event.data.toString() === 'checkAll';
 
+            let edited = false;
+
             /**
              * @type {import('telegram/tl/api').Api.TypeKeyboardButtonRow[]}
              */
@@ -23,10 +28,23 @@ module.exports = [
 
             for (let i = 0; i < rows.length; i += 1) {
                 if (rows[i].buttons[0].data.toString().match(/^li:\d+$/)) {
+                    edited = edited
+                        || (checkAll && rows[i].buttons[0].text.match(rU))
+                        || (!checkAll && rows[i].buttons[0].text.match(rC));
+
                     rows[i].buttons[0].text = checkAll
                         ? rows[i].buttons[0].text.replace(/^./, C)
                         : rows[i].buttons[0].text.replace(/^./, U);
                 }
+            }
+
+            if (!edited) {
+                event.answer({
+                    message: `All items already ${checkAll ? 'checked!' : 'unchecked!'}`,
+                    alert: true,
+                });
+
+                return;
             }
 
             await msg.edit({
